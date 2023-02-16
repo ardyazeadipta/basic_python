a317c82e6d538b064cfcf2c70d7b7ad208f9c38b
Ardya Inderawan <ardya.paradipta@gmail.com>
2023-02-16 14:48:04 +0700

    Add files via upload

---
+++ RealPython/review3_3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 14:36:01 2023
+
+@author: ardya
+"""
+
+print("My name is Ardya Paradipta Sylvano Inderawan.")
+
+name = "Ardya Paradipta Sylvano Inderawan"
+print(f"My name is {name}.")
