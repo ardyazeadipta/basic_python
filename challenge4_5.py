17d8e82b8cf0b1c1a930cc378c8c1bbd847a51f7
Ardya Inderawan <ardya.paradipta@gmail.com>
2023-02-19 11:57:40 +0700

    Add files via upload

---
+++ challenge4_5.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 11:54:15 2023
+
+@author: Ardya
+"""
+password = input("Tell me your password: ")
+print(f"The first letter of your password is {password[0]}")
