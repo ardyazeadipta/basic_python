8e873cd7e61f7db531065d1321b1a326a84a7d4f
Ardya Inderawan <ardya.paradipta@gmail.com>
2023-02-19 11:58:37 +0700

    Delete challenge4_5.py

--- challenge4_5.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 19 11:54:15 2023
-
-@author: Ardya
-"""
-password = input("Tell me your password: ")
-print(f"The first letter of your password is {password[0]}")
